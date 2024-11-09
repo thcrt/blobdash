32ee56e796a9d9a0aa8d00f4d8bbfac46009b460
thcrt <110127860+thcrt@users.noreply.github.com>
2024-11-09 13:56:58 +0100

    Allow custom configuration with blobdash.toml

--- src/blobdash/__init__.py
+++ src/blobdash/__init__.py
@@ -1,11 +1,20 @@
 import flask as f
-
+from click import secho
+import jinja2
+import tomllib
 
 def create_app():
     app = f.Flask(__name__)
 
+    try:
+        with open("blobdash.toml", "rb") as settings_file:
+            app.settings = tomllib.load(settings_file)
+    except FileNotFoundError:
+        secho("Config file `blobdash.toml` wasn't found!", bold=True, fg="bright_red")
+        raise SystemExit(1)
+
     @app.route("/")
     def index():
-        return f.render_template("index.html")
+        return f.render_template("index.html.jinja", settings=app.settings)
 
     return app
