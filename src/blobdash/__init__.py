106cac818eda47afafd5f000c2156ab43ec47066
thcrt <110127860+thcrt@users.noreply.github.com>
2024-12-03 23:19:09 +0100

    Don't make `settings` an attribute of `app`. Why did we do that in the first place?

--- src/blobdash/__init__.py
+++ src/blobdash/__init__.py
@@ -11,10 +11,10 @@ def create_app():
     app = f.Flask(__name__)
 
     try:
-        app.settings = Settings()
+        settings = Settings()
         if app.debug:
             secho("Loaded configuration:", bold=True)
-            print(app.settings.model_dump_json(indent=2))
+            print(settings.model_dump_json(indent=2))
     except ValidationError as e:
         secho("Configuration file was missing or had an error!", bold=True, fg="bright_red")
         secho(e)
@@ -22,14 +22,14 @@ def create_app():
 
     @app.route("/")
     def index():
-        if app.settings.auth.enabled:
-            user = f.request.headers.get(app.settings.auth.header)
-            user = app.settings.auth.default_user if user is None else user
+        if settings.auth.enabled:
+            user = f.request.headers.get(settings.auth.header)
+            user = settings.auth.default_user if user is None else user
 
-        return f.render_template("index.html.jinja", settings=app.settings, user=user)
+        return f.render_template("index.html.jinja", settings=settings, user=user)
 
     @app.route("/about")
     def about():
-        return app.settings.about
+        return settings.about
 
     return app
